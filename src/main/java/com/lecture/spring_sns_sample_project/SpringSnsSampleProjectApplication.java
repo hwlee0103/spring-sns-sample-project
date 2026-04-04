e05775e02ad3fa69bb60095362cb31b5c2f30974
hwlee0103 <sdiverHyeWon0103@gmail.com>
2026-04-04 23:28:58 +0900

    Initial Commit

---
+++ src/main/java/com/lecture/spring_sns_sample_project/SpringSnsSampleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.lecture.spring_sns_sample_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSnsSampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSnsSampleProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lecture/spring_sns_sample_project/SpringSnsSampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lecture.spring_sns_sample_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSnsSampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
